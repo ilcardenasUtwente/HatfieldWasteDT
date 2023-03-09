4259d6c33d0c44bf3072aa4a9768f2a75286cf95
ilcardenasUtwente <i.l.cardenasleon@student.utwente.nl>
2023-03-09 22:13:37 +0100

    Restart idea

--- TEST/Dash_test.py
+++ TEST/Dash_test.py
@@ -1,65 +1,31 @@
-import dash
-from dash import dcc
-from dash import html
-from dash import dash_table
-from dash.dash_table.Format import Group 
+from dash import Dash, html, dcc
+import plotly.express as px
 import pandas as pd
-import plotly.graph_objs as go
 
-app = dash.Dash()
+app = Dash(__name__)
 
-# Define the data that we want to display on the map and table
-data = pd.DataFrame({
-    'City': ['San Francisco', 'New York', 'Los Angeles'],
-    'Lat': [37.7749, 40.7128, 34.0522],
-    'Lon': [-122.4194, -74.0060, -118.2437],
-    'Population': [884363, 8336817, 3977683]
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
 })
 
-# Define the layout of the app
-app.layout = html.Div([
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for your data.
+    '''),
+
     dcc.Graph(
-        id='my-map',
-        figure={
-            'data': [{
-                'type': 'scattermapbox',
-                'lat': data['Lat'],
-                'lon': data['Lon'],
-                'mode': 'markers',
-                'marker': {
-                    'size': 14
-                },
-                'text': data['City']
-            }],
-            'layout': {
-                'mapbox': {
-                    'accesstoken': 'pk.eyJ1IjoiY3lnbnVzMjYiLCJhIjoiY2s5Z2MzeWVvMGx3NTNtbzRnbGtsOXl6biJ9.8SLdJuFQzuN-s4OlHbwzLg',
-                    'center': {'lat': 37.7749, 'lon': -122.4194},
-                    'zoom': 4
-                },
-                'margin': {'l': 0, 'r': 0, 't': 0, 'b': 0},
-                'height': 500
-            }
-        }
-    ),
-    """"dash_table.DataTable(
-        id='my-table',
-        columns=[{"name": i, "id": i} for i in data.columns],
-        data=data.to_dict('records')
-    )"""
+        id='example-graph',
+        figure=fig
+    )
 ])
 
-# Define the callbacks that update the table based on user interactions with the map
-@app.callback(
-    dash.dependencies.Output('my-table', 'data'),
-    [dash.dependencies.Input('my-map', 'clickData')])
-def update_table(clickData):
-    if clickData is not None:
-        city = clickData['points'][0]['text']
-        filtered_data = data[data['City'] == city]
-        return filtered_data.to_dict('records')
-    else:
-        return data.to_dict('records')
-
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run_server(debug=True)
